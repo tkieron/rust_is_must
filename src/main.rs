40700065f69c35f35b1665bd5a410571712a1c24
tkieron <tomasz@kieronski.pl>
2023-06-01 09:16:52 +0200

    1 - passing arg by value and by reference, basic loops example

--- src/main.rs
+++ src/main.rs
@@ -41,118 +41,62 @@ and using a const allows for optimizations like constant propagation not only in
 #[allow(path_statements)]
 #[allow(unused_mut)]
 fn main() {
-    // declared c1 as immutable integer size 8bits
-    let c1: i8 = 1;
-    // declared c2 as mutable integer size 16bits
-    let c2: i16 = 2;
-    // declared c3 as immutable integer size 128bits - max size of integer in rust
-    let c3: i128 = 3;
-    // declared c4 as immutable unsigned integer size 8bits
-    let u1: u8 = 1;
-    // by default type of a variable can be infered by compiler
-    let inf = 10;
-    // but it can be also declared explicitly
-    let inf_2 = 34.20;
-    // to improve readabilty number can be written with underscore separator
-    let readable_number = 1_000_000; // one milion
+    let mut nums = [1, 2, 3, 4, 5];
+    println!("First assigment {:?}", nums);
+    // change the value of first element is possible while it's declared as mutable
+    nums[0] = 0;
+    println!("After zero element is changed {:?}", nums);
+    // assigned first element reference to mutable variable
+    let mut number = &mut nums[0];
+    // passing the variable as mutable to function
+    by_reference(&mut number);
+    println!("After function was called with mutable variable {:?}", nums);
+    by_value(nums[3]);
+    println!("After function was called with mutable variable {:?}", nums);
 
-    // example of boolean variable
-    let is_real = true;
-    // string can be represented as a slice of bytes
-    let name = "John";
-    // or as a String object
-    let last_name = String::from("Doe");
-    // string can be concatenated with + operator
-    let full_name = name.to_owned() + " " + &last_name;
-    // or with format! macro
-    let full_name_2 = format!("{} {}", name, last_name);
-    // or with push_str method
-    let mut full_name_3 = String::from("John");
-    full_name_3.push_str(" Doe");
-    // or with push method - it takes only one character
-    let mut full_name_4 = String::from("John");
-    full_name_4.push(' ');
-
-    // tuple is a collection of values of different types
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    // tuple can be destructured
-    let (x, y, z) = tup;
-    // or accessed by index
-    let five_hundred = tup.0;
-    let six_point_four = tup.1;
-    let one = tup.2;
-
-    // array is a collection of values of the same type
-    let a = [1, 2, 3, 4, 5];
-    // array can be destructured
-    let [a1, b2, c3, d4, e5] = a;
-    // or accessed by index
-    let one = a[0];
-    let two = a[1];
-    let three = a[2];
-    let four = a[3];
-    let five = a[4];
-
-    // function can be called with or without parentheses
-    hello_no_args();
-    hello_no_args;
-    // function can be called with or without arguments
-    hello("John");
-    hello;
-    // function can be called with or without return value
-    let result = hello("John");
-    let result = hello;
-
-    // mutable variable can be reassigned
-    let mut some_mutable: u8 = 1;
-    some_mutable = 2;
-    println!("some_mutable is {}", some_mutable);
-
-    // shadowing - reusing the same name for variable
-    let shadowed = 1;
-    let shadowed = 2;
-    println!("shadowed is {}", shadowed);
-
-    // frozen - immutable variable cannot be reassigned
-    let mut frozen = 1;
-    {
-        // variable frozen is shadowed in this scope
-        let frozen = 2;
-        // now frozen is immutable
-        // frozen = 3; // this will not compile
-        println!("frozen is {}", frozen);
+    println!("for loop");
+    for i in 0..10 {
+        println!("{}",i);
     }
-    println!("frozen is {}", frozen);
-
-    // constants are immutable by default
-    // constants cannot be shadowed
-    // constants must be declared with type
-    // constants can be declared in any scope
-    // constants can be declared only with const keyword
-    // constants can be declared only with uppercase letters
-    // constants can be declared only with underscore separator for readability
-    // constants can be declared only with explicit type
-    // constants can be declared only with value assigned
-    // PI = 3.15; // this will not compile
-    // MAX_POINTS = 10;
-    println!("PI is {}", PI);
-    println!("MAX_POINTS is {}", MAX_POINTS);
-    // static global variables are immutable by default
-    // static global variables cannot be shadowed
-    // let MAX_POINTS: int32 = 100_000_000;
-
-    // use static mutable requires code block with unsafe
-    unsafe {
-        COUNTER += 1;
+    println!("while loop");
+    let mut j =  0;
+    while j < 10 {
+        println!("{}", j);
+        j = j + 1;
+    }
+    let mut k = 0;
+    println!("loop ");
+    loop {
+        println!("{}", k);
+        k = k + 1;
+        if k == 10 {
+            break;
+        }
     }
 }
 
-fn hello(name: &str) -> String {
-    format!("Hello, {}!", name)
+/*
+ Passing by reference.
+ This method takes mutable i32 as a paremeter,
+ prints it into console log and then sets it's value by changing memory addres to value 2
+ then one more time prints the value to console log
+ As the result not only value within the function has changed but also the value that was passed a function
+ */
+fn by_reference(number: &mut i32) {
+    println!("Method by_reference. Argument is {}", number);
+    // change of the value stored in-memory - we do it with pointer (*)
+    *number = 2;
+    println!("Method by_reference...and now Argument is {}", number);
 }
 
-fn hello_no_args() -> String {
-    format!("Hello, world!")
+/*
+    Passing by value. Value cannot be changed
+ */
+fn by_value(number: i32) {
+    println!("Method by_value. Argument is {}", number);
+    // cannot reasigne the value becouse the number is immutable
+    // number = 10;
+    // println!("Method by_value...and now Argument is {}", number);
 }
 
 
