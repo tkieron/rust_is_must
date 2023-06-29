9e824412c31dcad9c22e7bd4834021f0adce2c7a
tkieron <tomasz@kieronski.pl>
2023-06-29 09:12:37 +0200

    3 - match statement (switch in other languages)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use crate::Suit::{Clubs, Diamonds, Hearts, Spades};
 
 #[allow(unused_variables)]
 #[allow(unused_imports)]
@@ -6,16 +6,41 @@ use rand::Rng;
 #[allow(path_statements)]
 #[allow(unused_mut)]
 fn main() {
-    let mut rng = rand::thread_rng();
-    let mut x = rng.gen_range(-100..100);
-    if x >= 0 {
-        println!("{} is positive", x);
-    } else if x == 0 {
-        println!("{} is zero", x);
-    } else if x == 1 {
-        println!("{} is one", x);
-    } else {
-        println!("{} is negative", x);
+    print_choice(Hearts);
+    print_choice(Spades);
+    print_choice(Diamonds);
+    print_choice(Clubs);
+
+    country(44);
+    country(34);
+    country(125);
+    country(-50);
+}
+
+fn country(code: i32) {
+    let country = match code {
+        44 => "UK",
+        34 => "Spain",
+        7 => "RuZZia",
+        1..=990 => "Unknown",
+        _ => "Invalid"
+    };
+    println!("the country with code {} is {}", code, country);
+}
+
+enum Suit {
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
+
+fn print_choice(choice: Suit) {
+    match choice {
+        Hearts => println!("\u{2665}"),
+        Clubs => println!("\u{2663}"),
+        Diamonds => println!("\u{2666}"),
+        Spades => println!("\u{2660}"),
     }
 }
 
