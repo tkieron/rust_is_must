ebc35e91a314f96c1f0bce54cff097eaf60d5018
tkieron <tomasz@kieronski.pl>
2023-10-13 13:22:22 +0200

    HoFs

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,57 @@
 #[allow(path_statements)]
 #[allow(unused_mut)]
 fn main() {
-    println!("get squares");
-    get_squares(3478);
-    println!("get cubes");
-    get_cubes(4938);
-}
+    let squere = |a : i32| -> i32 { a * a };
+    apply(squere, 10);
 
-fn get_squares(limit : i32) {
-    let mut x = 1;
-    // this loop will execute as long limit is reached
-    while x * x < limit {
-        println!("{0} * {0} = {1}", x, x * x);
-        x += 1;
-    }
-}
-
-fn get_cubes(limit: i32) {
-    let mut x = 1;
-    // this loop will execute infinitely until break is called
-    loop {
-        println!("{0} * {0} * {0} = {1}", x, x * x * x);
-        x += 1;
-        if x * x * x > limit {
+    // Calculate the sum of all the squares less then 500
+    // only for even numbers
+    let mut sum = 0;
+    for i in 0.. {
+        let res  = squere(i);
+        if res > 500 {
             break;
         }
+        if !is_even(res) {
+            continue;
+        }
+        sum += res;
     }
+    println!("Sum is {0}", sum);
+
+    // Calculate the sum of all the squares less then 500
+    // only for even numbers
+    // using Higher Order Functions
+    let sum2 = (0..)
+        .map(|x| squere(x))
+        .take_while(|&x| x < 500)
+        .filter(|x| is_even(*x))
+        .fold(0, |sum, x| sum + x);
+    println!("Sum2 is {0}", sum2);
+
+    println!("factorial result of {0} is {1}", 5, factorial(5));
+    let sum3 = (0..)
+        .map(|x| factorial(x))
+        .take_while(|&x| x < 500)
+        .filter(|x| is_even(*x))
+        .fold(0, |sum,x| sum + x);
+    println!("Sum3 is {0}", sum3);
+
 }
+
+// calculate factorial of given number
+fn factorial(n: i32) -> i32 {
+    (1..n+1).fold(1, |a, b| a * b)
+}
+
+fn is_even(res: i32) -> bool {
+    res % 2 == 0
+}
+
+fn apply(f: fn(i32) -> i32, a: i32) {
+    println!("Result is {0}", f(a));
+}
+
 
 
 
